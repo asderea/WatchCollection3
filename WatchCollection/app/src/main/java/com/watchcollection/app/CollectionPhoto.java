f64084319cef0e8869571ee6b5ab63b17ded674f
SC <128973836+asderea@users.noreply.github.com>
2026-09-21 23:44:58 +0900

    Delete WatchCollection directory

--- WatchCollection/app/src/main/java/com/watchcollection/app/CollectionPhoto.java
+++
@@ -1,8 +0,0 @@
-package com.watchcollection.app;
-
-public class CollectionPhoto {
-    public long id;
-    public String originalPath = "";
-    public String displayPath = "";
-    public int displayOrder = 0;
-}
